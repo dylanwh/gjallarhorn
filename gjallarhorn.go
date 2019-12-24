bbb3cb11c22e73a818e86ef14edaf62238ac900b
Dylan Hardison <dylan@hardison.net>
2019-12-24 15:13:36 -0500

    more hacking

--- gjallarhorn.go
+++ gjallarhorn.go
@@ -2,27 +2,46 @@ package main
 
 import (
 	"fmt"
+	"github.com/pborman/getopt/v2"
 	"log"
 	"net"
 	"os"
+	"strings"
+    "context"
 )
 
-var _, uniqueLocalNetwork, _ = net.ParseCIDR("fc00::/7")
-var _, linkLocalNetwork, _ = net.ParseCIDR("fe80::/10")
-var _, legacyNetwork, _ = net.ParseCIDR("0.0.0.0/0")
+var domainFlag = getopt.StringLong("domain", 'd', "invalid", "the base domain used to fully qualify hostnames.")
 
 func main() {
+    ctx := context.Background()
+	getopt.Parse()
+	hostname := findPublicHostname(*domainFlag)
+	publicIPs := findPublicIPs()
+    resolvedIPs, err := net.DefaultResolver.LookupIPAddr(ctx, hostname)
+    if err != nil {
+        log.Print(fmt.Errorf("gjallarhorn: %v\n", err.Error()));
+        return
+    }
+    fmt.Printf("publicIPs: %v\nresolvedIPs: %v\n", publicIPs, resolvedIPs)
+
+}
+
+func findPublicHostname(domain string) string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	return strings.SplitN(hostname, ".", 2)[0] + "." + domain
+}
+
+func findPublicIPs() []net.IP {
+	var ips []net.IP
+
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		log.Print(fmt.Errorf("gjallarhorn: %v\n", err.Error()))
-		return
+		return ips
 	}
-
-	hostname, err := os.Hostname()
-	if err != nil {
-		log.Print(fmt.Errorf("gjallarhorn: %v\n", err.Error()))
-	}
-	fmt.Printf("Hostname is %s\n", hostname)
 
 	for _, iface := range ifaces {
 		if skipInterface(iface) {
@@ -42,14 +61,20 @@ func main() {
 			if skipIP(ip) {
 				continue
 			}
-			fmt.Printf("ip = %v\n", ip)
+			ips = append(ips, ip)
 		}
 	}
+
+	return ips
 }
 
 func skipInterface(iface net.Interface) bool {
 	return iface.Flags&net.FlagLoopback != 0 || iface.Flags&net.FlagPointToPoint != 0
 }
+
+var _, uniqueLocalNetwork, _ = net.ParseCIDR("fc00::/7")
+var _, linkLocalNetwork, _ = net.ParseCIDR("fe80::/10")
+var _, legacyNetwork, _ = net.ParseCIDR("0.0.0.0/0")
 
 func skipIP(ip net.IP) bool {
 	return uniqueLocalNetwork.Contains(ip) || linkLocalNetwork.Contains(ip) || legacyNetwork.Contains(ip)
