c242f90cc9f0e1de0c77b0313daf2deb2e9d308c
Dylan Hardison <dylan@hardison.net>
2019-12-28 10:13:31 -0500

    no output

--- cmd/gjallarhorn/main.go
+++ cmd/gjallarhorn/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 
 	"github.com/dylanwh/gjallarhorn/config"
 	"github.com/dylanwh/gjallarhorn/message"
@@ -14,11 +13,8 @@ func main() {
 	cfg.CheckArgs()
 	msg := message.New(cfg)
 	ua := useragent.New(cfg)
-	resp, err := ua.Send(msg)
+	_, err := ua.Send(msg)
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
-	buf, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf("%s\n", string(buf))
-
 }
